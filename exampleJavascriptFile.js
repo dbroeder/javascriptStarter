01247f7b34e0406b874f8fbc16ca508fd27b9d7c
dbroeder <dbroeder@4rhuskies.org>
2020-02-10 08:25:06 -0600

    Add files via upload

---
+++ exampleJavascriptFile.js
@@ -0,0 +1,9 @@
+
+//function that connects to button in exampleHTML.html file
+function someJavascriptFunction(){
+    //variable that stores the input value gotten from the exampleHTML.html file. 
+    let textFromInput = document.getElementById('userInput').value;
+    
+    //shows the value stored in the variable to the console. 
+    console.log(textFromInput);
+}
